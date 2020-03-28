2f70c6a0d97c11275eee1c6778afb167321d47eb
Bartolomeo.a52f <bart.nicolott@libero.it>
2020-03-28 15:37:49 +0100

    bart devenv trick

--- bartdevenv.sh
+++ bartdevenv.sh
@@ -2,13 +2,20 @@
 #Android Studio and Gradle are using different locations for the JDK.
 #				Android Studio: /usr/local/java/jdk1.8.0_144
 #				Gradle: /usr/local/android-studio/jre
-cat gradle.properties  | sed s/#org.gradle.java/org.gradle.java/ >gradle.properties
+cat gradle.properties  | sed s/#org.gradle.java/org.gradle.java/ >gradle.properties.txt
+mv gradle.properties.txt gradle.properties
 #couldn't fix this error otherwise:
 # 
-cat app/build.gradle  | sed s/\\compileOptions/compileOptions/ >app/build.gradles
-cat app/build.gradle  | sed s/\\    sourceCompatibility/    sourceCompatibility/ >app/build.gradles
-cat app/build.gradle  | sed s/\\    targetCompatibility/    targetCompatibility/ >app/build.gradles
-cat app/build.gradle  | sed s/\\compileOptions}/}/ >app/build.gradle
+cat app/build.gradle  | sed s/\\\/\\\/compileOptions/compileOptions/ > app/build.gradle.txt
+mv app/build.gradle.txt  app/build.gradle
 
-org.koin.core.error.NoBeanDefFoundException: No definition found for 
-class:'org.coepi.android.repo.SymptomRepo'. Check your definitions!
+#cat app/build.gradle  | sed "s/\\\/\\\/ +sourceCompatibility/    sourceCompatibility/" >app/build.gradle.txt
+#mv app/build.gradle.txt  app/build.gradle
+
+#cat app/build.gradle  | sed s/\\\/\\\/    targetCompatibility/    targetCompatibility/ >app/build.gradle.txt
+#mv app/build.gradle.txt  app/build.gradle
+
+#cat app/build.gradle  | sed s/\\\/\\\/compileOptions\\\}/\\\}/ >  app/build.gradle.txt
+#mv app/build.gradle.txt  app/build.gradle
+
+
