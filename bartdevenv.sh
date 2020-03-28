1e7e343b90f5b474de292b42cb3177d00de93f90
Bartolomeo.a52f <bart.nicolott@libero.it>
2020-03-28 15:17:53 +0100

    bartdevenv tricks

--- bartdevenv.sh
+++ bartdevenv.sh
@@ -2,13 +2,13 @@
 #Android Studio and Gradle are using different locations for the JDK.
 #				Android Studio: /usr/local/java/jdk1.8.0_144
 #				Gradle: /usr/local/android-studio/jre
-cat gradle.properties  | sed s/#org.gradle.java/org.gradle.java/ >gradle.properties.txt
+cat gradle.properties  | sed s/#org.gradle.java/org.gradle.java/ >gradle.properties
 #couldn't fix this error otherwise:
 # 
-cat build.gradles  | sed s/\\compileOptions/compileOptions/ >gradle.properties.txt
-cat build.gradles  | sed s/\\    sourceCompatibility/    sourceCompatibility/ >gradle.properties.txt
-cat build.gradles  | sed s/\\    targetCompatibility/    targetCompatibility/ >gradle.properties.txt
-cat build.gradles  | sed s/\\compileOptions}/}/ >gradle.properties.txt
+cat app/build.gradle  | sed s/\\compileOptions/compileOptions/ >app/build.gradles
+cat app/build.gradle  | sed s/\\    sourceCompatibility/    sourceCompatibility/ >app/build.gradles
+cat app/build.gradle  | sed s/\\    targetCompatibility/    targetCompatibility/ >app/build.gradles
+cat app/build.gradle  | sed s/\\compileOptions}/}/ >app/build.gradle
 
 org.koin.core.error.NoBeanDefFoundException: No definition found for 
 class:'org.coepi.android.repo.SymptomRepo'. Check your definitions!
